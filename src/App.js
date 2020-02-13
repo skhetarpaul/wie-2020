f89031a8ecc0e20031c3c8ad7238dcef2d9b1895
Saransh <saranshkhetarpaul99@gmail.com>
2020-02-13 19:41:50 +0530

    navbar added -sk

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import Navbar from "./components/Navbar";
+import Section from "./components/Section";
+import dummyText from "./DummyText";
+// import Hero from './images/woman.png';
+
 
 function App() {
   return (
     <div className="App">
+      
       <div className = "animation-moving">
+      <Navbar />
+      
+      {/* <img src={Hero} /> */}
+        {/* <Section
+          title="Section 1"
+          subtitle={dummyText}
+          dark={true}
+          id="section1"
+        />
+        <Section
+          title="Section 2"
+          subtitle={dummyText}
+          dark={true}
+          id="section2"
+        />
+        <Section
+          title="Section 3"
+          subtitle={dummyText}
+          dark={true}
+          id="section3"
+        />
+        <Section
+          title="Section 4"
+          subtitle={dummyText}
+          dark={true}
+          id="section4"
+        />
+        <Section
+          title="Section 5"
+          subtitle={dummyText}
+          dark={true}
+          id="section5"
+        /> */}
         <ul className = "box-type">
           <li></li>
           <li></li>
