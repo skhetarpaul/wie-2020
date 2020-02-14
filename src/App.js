557be04353aeaf37e9b9d01cc299847e676ed21e
Saransh <saranshkhetarpaul99@gmail.com>
2020-02-14 16:54:41 +0530

    added image and wie hackathon

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React,{Component} from 'react';
 import './App.css';
 import Navbar from "./components/Navbar";
 import Section from "./components/Section";
-// import dummyText from "./DummyText";
-
+import woman from '../src/images/wom-p2.webp';
+// import singlePage from './components/singlePage'
+import Texter from './components/Texter'
 function App() {
   return (
     <div className="App">
       <Navbar />
-      
-      <div className = "animation-moving">
+      <div className = "woman-image">
+        <img src = {woman} />
+      </div>
+      <div className = "float-wie">
+        <p className = "wie-hack"> <span className = "enlarge">{` <`}</span><Texter />
+        <span className = "enlarge">{` />`}</span></p>
+        <div className = "animation-moving">
+      </div>
       
          {/* <Section
           title="Section 1"
