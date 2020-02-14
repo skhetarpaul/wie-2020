8623ad1b1a6152518cbae1ac00ce8fc3e2eb22a4
Saransh <saranshkhetarpaul99@gmail.com>
2020-02-14 12:00:40 +0530

    removed repo redundancies

--- src/App.js
+++ src/App.js
@@ -2,48 +2,22 @@ import React,{Component} from 'react';
 import './App.css';
 import Navbar from "./components/Navbar";
 import Section from "./components/Section";
-import dummyText from "./DummyText";
-// import Hero from './images/woman.png';
-
+// import dummyText from "./DummyText";
 
 function App() {
   return (
     <div className="App">
-      
-      <div className = "animation-moving">
       <Navbar />
       
-      {/* <img src={Hero} /> */}
-        {/* <Section
+      <div className = "animation-moving">
+      
+         {/* <Section
           title="Section 1"
           subtitle={dummyText}
           dark={true}
           id="section1"
-        />
-        <Section
-          title="Section 2"
-          subtitle={dummyText}
-          dark={true}
-          id="section2"
-        />
-        <Section
-          title="Section 3"
-          subtitle={dummyText}
-          dark={true}
-          id="section3"
-        />
-        <Section
-          title="Section 4"
-          subtitle={dummyText}
-          dark={true}
-          id="section4"
-        />
-        <Section
-          title="Section 5"
-          subtitle={dummyText}
-          dark={true}
-          id="section5"
         /> */}
+       
         <ul className = "box-type">
           <li></li>
           <li></li>
