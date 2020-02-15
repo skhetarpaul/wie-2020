0526b7dbedab589f9c392d47890217aa59020184
Saransh <saranshkhetarpaul99@gmail.com>
2020-02-15 18:52:37 +0530

    changed navbar menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,86 +1,98 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 import logo from "../logo.svg";
-import { Link, animateScroll as scroll } from "react-scroll";
+import './Navbar.css'
+import {FaAlignRight} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+
 
 export default class Navbar extends Component {
-  scrollToTop = () => {
-    scroll.scrollToTop();
-  };
+    state = {
+        isOpen: false
+    }
 
-  render() {
-    return (
-      <nav className="nav" id="navbar">
-        <div className="nav-content">
-          <img
-            src={logo}
-            className="nav-logo"
-            alt="Logo"
-            onClick={this.scrollToTop}
-          />
-          <ul className="nav-items">
-            <li className="nav-item">
-              <Link
-                activeClass="active"
-                to="section1"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link 
-                activeClass="active"
-                to="section2"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                About Us
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                activeClass="active"
-                to="section3"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                Judges
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                activeClass="active"
-                to="section4"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                <button className = "btn-primary">Register</button>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                activeClass="active"
-                to="section4"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                <button className = "btn-primary">Contact Us</button>
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
+    handleToggle=() => {
+        this.setState({isOpen: !this.state.isOpen})
+    }
+
+    render() {
+        return (
+            <nav className = "navbar">
+                <div className = "nav-center">
+                    <div className = "nav-header">
+                        <Link to = "/">
+                            <img src = {logo} className = "navbar-img" alt = "WIE"></img>
+                        </Link>
+                        <button type = "button" className = "nav-btn" 
+                         onClick = {this.handleToggle}>
+                            <FaAlignRight className = "nav-icon"/>
+                        </button>
+                    </div>
+                    
+                    <ul className = {this.state.isOpen? "nav-links show-nav":
+                        "nav-links"}>
+                      <li className="nav-item">
+                        <Link
+                          activeClass="active"
+                          to="section1"
+                          spy={true}
+                          smooth={true}
+                          offset={-70}
+                          duration={500}
+                        >
+                          Home
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link 
+                          activeClass="active"
+                          to="section2"
+                          spy={true}
+                          smooth={true}
+                          offset={-70}
+                          duration={500}
+                        >
+                          About Us
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link
+                          activeClass="active"
+                          to="section3"
+                          spy={true}
+                          smooth={true}
+                          offset={-70}
+                          duration={500}
+                        >
+                          Judges
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link
+                          activeClass="active"
+                          to="section4"
+                          spy={true}
+                          smooth={true}
+                          offset={-70}
+                          duration={500}
+                        >
+                          <button className = "btn-primary">Register</button>
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link
+                          activeClass="active"
+                          to="section4"
+                          spy={true}
+                          smooth={true}
+                          offset={-70}
+                          duration={500}
+                        >
+                          <button className = "btn-primary">Contact Us</button>
+                        </Link>
+                      </li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
 }
